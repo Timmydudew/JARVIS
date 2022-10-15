2671fe7ce1697215af3f2a5e1b787b58d7ff6810
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-10-15 21:06:25 +0100

    Update Jar.py

--- Jar.py
+++ Jar.py
@@ -1,15 +1,90 @@
-import speech_recognition as sr
 import pyttsx3
-import pywhatkit
+import speech_recognition as sr
 import datetime
 import wikipedia
-import pyjokes
- listener = sr.Recognizer()
-engine = pyttsx3.init()
+import webbrowser as wb
+import os
+import smtplib
+import requests
+from pprint import pprint
+from selenium import webdriver
+
+ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id) 
-def talk(text): engine.say(text) engine.runAndWait() def take_command(): try: with sr.Microphone() as source: print('listening...') voice = listener.listen(source) command = listener.recognize_google(voice) command = command.lower() if 'alexa' in command: command = command.replace('alexa', '') print(command) except: pass return command def run_alexa(): command = take_command() print(command) if 'play' in command: song = command.replace('play', '') talk('playing ' + song) pywhatkit.playonyt(song) elif 'time' in command: time = datetime.datetime.now().strftime('%I:%M %p') talk('Current time is ' + time) elif 'who the heck is' in command: person = command.replace('who the heck is', '') info = wikipedia.summary(person, 1) print(info) talk(info) elif 'date' in command: talk('sorry, I have a headache') elif 'are you single' in command: talk('I am in a relationship with wifi') elif 'joke' in command: talk(pyjokes.get_joke()) else: talk('Please say the command again.') while True: run_alexa()
+#print(voices[1].id)
+engine.setProperty('voice',voices[0].id)
+ def speak(audio): 
+     engine.say(audio) 
+     engine.runAndWait() 
 
-Footer
+def wishMe(): 
+    speak("Welcome back sir") 
+    hour = int(datetime.datetime.now().hour) 
+    print(hour)
+    year = int(datetime.datetime.now().year) 
+    month = int(datetime.datetime.now().month) 
+    date = int(datetime.datetime.now().day) 
+    Time = datetime.datetime.now().strftime("%I:%M:%S") 
+    print(Time)
+    print(date) 
+    print(month) 
+    print(year) 
+    speak("the current Time is") 
+    speak(Time) 
+    speak("the current Date is") 
+    speak(date) 
+    speak(month)
+    speak(year) 
+    if hour>=6 and hour<12: 
+      speak("Good Morning Sir!") 
 
+    elif hour>=12 and hour<18: 
+      speak("Good Afternoon Sir!")
 
+    elif hour>=18 and hour<24: 
+      speak("Good Evening Sir") 
+
+    else: speak("Good Night Sir!") 
+
+    speak("Jarvis at your Service. Please tell me how can I help You ")
+   #wishMe()
+
+def takeCommand():
+    r = sr.Recognizer() 
+    with sr.Microphone() as source: 
+    print("i am Listening...") 
+    r.pause_threshold = 1 
+    audio = r.listen(source) 
+    try: print("Recognizing...") 
+    query = r.recognize_google(audio, language='en-in') 
+    print(f"Sir you Said:{query}\n") 
+    except Exception as e: 
+    print(e) 
+    print("Say that again Please...") 
+    speak("Say that again Please...") 
+    return "None" 
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587) 
+    server.ehlo() 
+    server.starttls() 
+    server.login('Senderemail@gmail.com', 'Password') 
+    server.sendmail('Senderemail@gmail.com', to, content) 
+    server.close() 
+
+def lighton(): 
+    driver = webdriver.Chrome('C:/Users/Username/Downloads/chromedriver.exe') 
+    add the location of the chrome Drivers driver.get("https://Add here.000webhostapp.com/main.html")
+    Add the webhost name elem1 = driver.find_element_by_id("S1off") 
+    elem1.click()
+ 
+def lightoff(): 
+    driver = webdriver.Chrome('C:/Users/timifresh101/Downloads/chromedriver.exe') 
+    driver.get("https://Add here.000webhostapp.com/main.html")
+    Add the webhost name elem1 = driver.find_element_by_id("S1on") 
+    elem1.click() 
+    if __name__ == "__main__": 
+    wishMe() 
+    while True:
+   query = takeCommand().lower() if 'wikipedia' in query:
